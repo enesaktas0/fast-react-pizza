da98c6b1dffb80876de988d4c708e911d4f0b256
enesaktas0 <126340173+enesaktas0@users.noreply.github.com>
2023-12-13 15:36:42 +0300

    Update Header.jsx

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -7,7 +7,7 @@ export default function Header() {
   return (
     <header className="border-radi flex justify-center border-b border-stone-200 bg-yellow-400 px-4 py-3 uppercase sm:px-6">
       <div className="flex w-full max-w-7xl  items-center justify-between">
-        <Link to="/" className="tracking-widest">
+        <Link to="/fast-react-pizza" className="tracking-widest">
           Fast React Pizza Co.
         </Link>
         <SearchOrder />
